9ebfa0d410fff5b539573644f35db4873c46e92e
takenncs <116774376+Takennncs@users.noreply.github.com>
2025-11-22 18:53:46 +0200

    Update config.php

--- steamauth/config.php
+++ steamauth/config.php
@@ -1,12 +1,12 @@
 <?php
-$steamauth['apikey'] = "058CE9F7A684D4D4B1780A16B68D78FF";
-$steamauth['domainname'] = "http://localhost";
+$steamauth['apikey'] = "";
+$steamauth['domainname'] = "";
 $steamauth['loginpage'] = "dashboard.php";
 $steamauth['logoutpage'] = "index.php";
 
-$host = "localhost";
-$dbname = "qbcore_21c90a"; 
-$username = "root";
+$host = "";
+$dbname = ""; 
+$username = "";
 $password = "";
 
 try {
