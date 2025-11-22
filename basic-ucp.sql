479579cf74a4f8eea26625c71f8af5be31d7fd0a
takenncs <116774376+Takennncs@users.noreply.github.com>
2025-11-22 18:52:30 +0200

    Update basic-ucp.sql

--- basic-ucp.sql
+++ basic-ucp.sql
@@ -10,3 +10,12 @@ CREATE TABLE `ucp_users` (
 ----
 ALTER TABLE `players`
 ADD COLUMN `steam` VARCHAR(255) NOT NULL;
+
+----
+CREATE TABLE `playtime` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `steam` varchar(64) NOT NULL,
+  `gametime` double NOT NULL DEFAULT 0,
+  `points` int(11) NOT NULL DEFAULT 0,
+  `last_updated` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
